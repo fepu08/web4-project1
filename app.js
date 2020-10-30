3adc4e8621eaee40badb3a34b43c850cdbdc8a72
fepu08 <besenyeiferenc@protonmail.com>
2020-10-30 10:35:47 +0100

    navbar
    navbar is working correctly

---
+++ app.js
@@ -0,0 +1,26 @@
+// UI Elements
+const nav = document.getElementById('side-nav');
+const showNav = document.getElementById('show-nav-btn');
+const hideNav = document.getElementById('hide-nav-btn');
+
+// Show nav
+showNav.addEventListener('click', () => {
+    //if small screen
+    if(window.matchMedia(("(max-width: 767px)")).matches || nav.style.transform === 'translateX(-100%)'){
+        nav.style.transform = 'translateX(0%)';
+    }
+    document.body.classList.add('show-nav');
+    showNav.classList.add('d-none');
+
+getTranslateX(nav);
+    /*document.body.classList.add('show-nav')
+    showNav.classList.add('d-none');*/
+});
+// Hide nav
+hideNav.addEventListener('click', () => {
+    if(window.matchMedia(("(max-width: 767px)")).matches || nav.style.transform === 'translateX(0%)'){
+        nav.style.transform = 'translateX(-100%)';
+    }
+    document.body.classList.remove('show-nav');
+    showNav.classList.remove('d-none');
+});
